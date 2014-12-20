444a6f3c9c0d51d7472f1b6a33e129891853db60
Gabriel Castro <gabriel_kstro@hotmail.com>
2014-12-20 18:37:03 -0300

    Classe Conexao criada

---
+++ C#/PetPamonha/PetPamonha/DAO/conexao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace PetPamonha.DAO
+{
+    class conexao
+    {
+        private MySqlConnection con;
+        String conexao = "server=localhost;database=petpamonha;uid=root;password=    ";
+
+        public MySqlConnection Con {
+            get {
+                return con = new MySqlConnection(this.conexao);            
+            }
+        }
+    }
+}
